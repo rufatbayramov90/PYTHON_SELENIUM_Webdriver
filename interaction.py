b1850223fdecc1f5a4a25ec642dd3e2fd92a53ac
rufatbayramov90 <rufatbyrmv@gmail.com>
2024-01-07 14:06:49 +0400

    Sending keyboard input to Selenium

--- interaction.py
+++ interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 # Keep Chrome browser open after program finishes
@@ -14,4 +15,15 @@ driver.get("https://en.wikipedia.org/wiki/Main Page")
 
 # Home in on anchor tag using CSS selectors
 article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(article_count.text)
+article_count.click()
+
+# Find element by Link Text
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+# Find the "Search" <input> by Name
+search = driver.find_element(By.NAME, value="search")
+
+
+# Sending keyboard input to Selenium
+search.send_keys("Python", Keys.ENTER)
