d0dcba7e21d2ddbe43e754d113f6450d55fb6e44
rufatbayramov90 <rufatbyrmv@gmail.com>
2024-01-07 13:34:48 +0400

    Home in on anchor tag using CSS selectors

---
+++ interaction.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# Create and configure the Chrome webdriver
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to Wikipedia
+driver.get("https://en.wikipedia.org/wiki/Main Page")
+
+# Home in on anchor tag using CSS selectors
+article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+print(article_count.text)
